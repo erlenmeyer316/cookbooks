68fbc6439a64978e6d34158c14e60c09b1e789cc
Adam M. Dickison <adam.dickison@posteo.net>
2024-12-18 15:54:59 -0500

    add fstab to filegen

--- shell/bash_kpxc-filegen/run.sh
+++ shell/bash_kpxc-filegen/run.sh
@@ -14,6 +14,19 @@ fi
 PW=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa password "${KPXC_VAULT}" "KeepassXC WebDAV")
 USER=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa username "${KPXC_VAULT}" "KeepassXC WebDAV")
 URL=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa url "${KPXC_VAULT}" "KeepassXC WebDAV")
-MNT=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa mount "${KPXC_VAULT}" "KeepassXC WebDAV")
-echo "${MNT}"
+MNT=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa mountpoint "${KPXC_VAULT}" "KeepassXC WebDAV")
+FS=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa filesystem "${KPXC_VAULT}" "KeepassXC WebDAV")
+OPTS=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa options "${KPXC_VAULT}" "KeepassXC WebDAV")
+DUMP=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa dump "${KPXC_VAULT}" "KeepassXC WebDAV")
+FSCK=$(echo "${KPXC_PASSWORD}" | keepassxc-cli show -qsa fsck "${KPXC_VAULT}" "KeepassXC WebDAV")
+
+if [ -f secretfile ]; then
+   rm secretfile
+fi
 printf "${URL}		${USER} ${PW}" > secretfile
+
+if [ -f fstab ]; then
+   rm fstab
+fi
+
+printf "${URL} ${MNT} ${FS} ${OPTS} ${DUMP} ${FSCK}" > fstab
